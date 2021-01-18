1f6ed85ef2c4a96173befba9b1a6b22194ceebd6
Zaphaan <jerome.przybylski@gmail.com>
2021-01-18 19:59:28 +0100

    Front : static - patients folder

--- Front/src/components/Patients/index.js
+++ Front/src/components/Patients/index.js
@@ -4,11 +4,15 @@ import React from 'react';
 // == Import
 import './patients.scss';
 
+// == Impot images
+import patient from 'src/assets/icones/patient2.svg'
+
 
 // == Composant
 const Patients = () => {
-  return <div className="atients">
-        TODO:PATIENTS
+  return <div className="patients">
+        <p className="patients-title">Patients</p>
+        <img src={patient} alt="patients" className="patients-img"/>
   </div>
 };
 
