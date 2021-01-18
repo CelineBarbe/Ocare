f97ad3ed76502c1b81371571e3a81b212ea70983
Zaphaan <jerome.przybylski@gmail.com>
2021-01-18 21:57:12 +0100

    Front : static - add tour folder component

--- Front/src/components/Tour/index.js
+++ Front/src/components/Tour/index.js
@@ -8,7 +8,35 @@ import './tour.scss';
 // == Composant
 const Tour= () => {
   return <div className="tour">
-        TODO:TOUR
+    <p className="tour-title">Ma tournée </p>
+    <ul className="tour-ul">
+      <li className="tour-li">
+        <span className="tour-span-date">06:00</span>
+        <span className="tour-span-name">Mr Pichon</span>
+        <span className="tour-span-tag">Pansement</span>
+      </li>
+      <li className="tour-li">
+        <span className="tour-span-date">07:00</span>
+        <span className="tour-span-name">Mme Paco</span>
+        <span className="tour-span-tag">Toilette</span>
+      </li>
+      <li className="tour-li">
+        <span className="tour-span-date">08:00</span>
+        <span className="tour-span-name">Mr Robillard</span>
+        <span className="tour-span-tag">Injection</span>
+      </li>
+      <li className="tour-li">
+        <span className="tour-span-date">09:00</span>
+        <span className="tour-span-name">Mme Vidal</span>
+        <span className="tour-span-tag">Prise de sang</span>
+      </li>
+      <li className="tour-li">
+        <span className="tour-span-date">10:00</span>
+        <span className="tour-span-name">Mr Frangin</span>
+        <span className="tour-span-tag">Tension</span>
+      </li>
+    </ul>
+
   </div>
 };
 
