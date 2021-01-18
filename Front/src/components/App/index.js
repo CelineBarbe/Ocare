3a12a4c7506aed2ec4ae034df61c2f15bf80bc8d
Zaphaan <jerome.przybylski@gmail.com>
2021-01-18 11:09:45 +0100

    Setup React model + init back and front folders

---
+++ Front/src/components/App/index.js
@@ -0,0 +1,18 @@
+// == Import npm
+import React from 'react';
+
+// == Import
+import './styles.scss';
+
+// == Import components
+import Header from 'src/components/Header';
+
+// == Composant
+const App = () => {
+  return (<div className="app">
+    <Header />
+  </div>)
+};
+
+// == Export
+export default App;
---
+++ Front/src/components/Header/index.js
@@ -0,0 +1,15 @@
+// == Import npm
+import React from 'react';
+
+// == Import
+import './styles.scss';
+
+// == Composant
+const Header = () => {
+  return <header className="header">
+        TODO: HEADER
+  </header>
+};
+
+// == Export
+export default Header;
