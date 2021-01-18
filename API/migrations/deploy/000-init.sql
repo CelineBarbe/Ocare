7c144b3382ad97aa59ade224a569a9a659e484c3
LindsayMourinet <lindsay.mourinet@icloud.com>
2021-01-18 14:44:38 +0100

    import some data to nurse table

--- API/migrations/deploy/000-init.sql
+++ API/migrations/deploy/000-init.sql
@@ -2,6 +2,14 @@
 
 BEGIN;
 
--- XXX Add DDLs here.
+CREATE TABLE nurse(
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    SIREN_code TEXT NOT NULL UNIQUE,
+    firstname TEXT NOT NULL,
+    lastname TEXT NOT NULL,
+    email TEXT NOT NULL,
+    password TEXT NOT NULL,
+    phone_number INT NOT NULL
+);
 
 COMMIT;
--- API/migrations/verify/000-init.sql
+++ API/migrations/verify/000-init.sql
@@ -2,6 +2,7 @@
 
 BEGIN;
 
--- XXX Add verifications here.
+INSERT INTO nurse(SIREN_code, firstname, lastname, email, password, phone_number)
+    VALUES('917222465', 'Olivier', 'Raynal', 'olivier@raynal.com', 'gyefgecz', 0612345678);
 
 ROLLBACK;
