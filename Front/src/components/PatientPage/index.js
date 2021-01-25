3d47fda4be0e58b0b0e7500ac6d06bdbe9cf0b4f
Zaphaan <jerome.przybylski@gmail.com>
2021-01-25 12:19:05 +0100

    Front: static - factorization logBook Component

--- Front/src/components/PatientPage/index.js
+++ Front/src/components/PatientPage/index.js
@@ -1,7 +1,7 @@
 // == Import npm
 import React, { useEffect, useState } from 'react';
 import {useParams} from 'react-router-dom';
-import Modal from 'react-modal';
+
 // == Import
 import './patientPage.scss';
 
@@ -11,14 +11,12 @@ import plus from 'src/assets/icones/plus2.svg';
 import wave from 'src/assets/icones/wave.svg';
 import pill from 'src/assets/icones/pill.svg';
 import bandage from 'src/assets/icones/bandage.svg';
-import close from 'src/assets/icones/close.svg';
+
 
 // == Import 
 import Header from 'src/containers/Header';
 import Nav from 'src/components/Nav';
-
-
-Modal.setAppElement('#root')
+import LogBook from 'src/components/LogBook';
 
 // == Composant
 const PatientPage = ({
@@ -66,7 +64,11 @@ const PatientPage = ({
   const [entryModal,setOpenEntry] = useState(false);
   
   function openModalPatient() {
-    setOpenPatient(true);
+    if (patientModal) {
+      setOpenPatient(false);
+    } else {
+      setOpenPatient(true);
+    }
   }
 
   function openModalEntry() {
@@ -82,7 +84,6 @@ const PatientPage = ({
  
   const modale = (
     <div className="modal-patient">
-      <img onClick={closeModalPatient} src={close} className="modal-patient-close" alt="close"/>
       <h3 className="modal-patient-title secondary">Etat Civil</h3> 
       <p className="modal-patient-address"> {gender} de {birthdate} ans. </p>
 
@@ -140,12 +141,7 @@ const PatientPage = ({
         Ajouter
       </button>
     </form>
-
-
     </div>
-
-
-
   )
   
   return (
@@ -165,95 +161,7 @@ const PatientPage = ({
                 <img onClick={openModalEntry} className="patient-add-care-img" src={plus} alt="ajouter"/>
                 <span onClick={openModalEntry} className="patient-add-care-title">Ajouter une entrée</span>
               </div>
-
-            
-            {/* ENTRY CONTAINER OF THE PAGE */} 
-              <div className="carnet-sante-container">
-                {entryModal ? modaleEntry : null}
-              {/* NEW ENTRY PART OF THE PAGE */}
-                <div className="carnet-sante-entry">
-                  <div className="carnet-sante-entry-top">
-                    <div className="carnet-sante-entry-top-icone">
-                      <img src={pill} className="carnet-sante-entry-top-icone-img" alt="icone"/>
-                    </div>
-                    <div className="carnet-sante-entry-top-care">
-                      <div className="carnet-sante-entry-top-care-top">
-                        <h3 className="carnet-sante-entry-top-care-top-title">Pilulier</h3>
-                        <span className="carnet-sante-entry-top-care-top-name">Jérôme</span>
-                      </div>
-                      <div className="carnet-sante-entry-top-care-bottom">
-                        <span className="carnet-sante-entry-top-care-bottom-date">22 Jan 2021</span>
-                      </div> 
-                    </div>
-                  </div>
-                  <div className="carnet-sante-entry-bottom">
-                    <div className="carnet-sante-entry-bottom-left">
-                      <div className="carnet-sante-entry-bottom-left-border"></div>
-                    </div>
-                    <div className="carnet-sante-entry-bottom-right">
-                      <span className="carnet-sante-entry-bottom-right-observation">
-                        Attention pansement qui coule, à vérifier tous les jours, on va essayer de mettre plein de texte pour voir comment réagit le container. Il faut encore mettre pas mal de texte pour que tout dépasse, il en faut encore encore et encore, c'est long j'aurai mieux faire de prendre un lorem ipsum automatiquement.
-                      </span>
-                    </div>
-                  </div>
-                </div>  
-
-              {/* NEW ENTRY PART OF THE PAGE WITHOUT OBSERVATION SPAN */}
-                <div className="carnet-sante-entry">
-                  <div className="carnet-sante-entry-top">
-                    <div className="carnet-sante-entry-top-icone">
-                      <img src={wave} className="carnet-sante-entry-top-icone-img" alt="icone"/>
-                    </div>
-                    <div className="carnet-sante-entry-top-care">
-                      <div className="carnet-sante-entry-top-care-top">
-                        <h3 className="carnet-sante-entry-top-care-top-title">Constante</h3>
-                        <span className="carnet-sante-entry-top-care-top-name">Marlène</span>
-                      </div>
-                      <div className="carnet-sante-entry-top-care-bottom">
-                        <span className="carnet-sante-entry-top-care-bottom-date">21 Jan 2021</span>
-                      </div> 
-                    </div>
-                  </div>
-                  <div className="carnet-sante-entry-bottom">
-                    <div className="carnet-sante-entry-bottom-left">
-                      <div className="carnet-sante-entry-bottom-left-border"></div>
-                    </div>
-                    <div className="carnet-sante-entry-bottom-right">
-                      {/* NO OBSERVATION PAGE IN BOTTOM-RIGHT FOR TEST*/}
-                    </div>
-                  </div>
-                </div>  
-
-                {/* NEW ENTRY PART OF THE PAGE WITH SMALL OBSERVATION SPAN */}
-                <div className="carnet-sante-entry">
-                  <div className="carnet-sante-entry-top">
-                    <div className="carnet-sante-entry-top-icone">
-                      <img src={bandage} className="carnet-sante-entry-top-icone-img" alt="icone"/>
-                    </div>
-                    <div className="carnet-sante-entry-top-care">
-                      <div className="carnet-sante-entry-top-care-top">
-                        <h3 className="carnet-sante-entry-top-care-top-title">Pansement</h3>
-                        <span className="carnet-sante-entry-top-care-top-name">Stéphane</span>
-                      </div>
-                      <div className="carnet-sante-entry-top-care-bottom">
-                        <span className="carnet-sante-entry-top-care-bottom-date">20 Jan 2021</span>
-                      </div> 
-                    </div>
-                  </div>
-                  <div className="carnet-sante-entry-bottom">
-                    <div className="carnet-sante-entry-bottom-left">
-                      <div className="carnet-sante-entry-bottom-left-border"></div>
-                    </div>
-                    <div className="carnet-sante-entry-bottom-right">
-                      <span className="carnet-sante-entry-bottom-right-observation">
-                          Attention pansement qui coule, à vérifier tous les jours.
-                      </span>
-                    </div>
-                  </div>
-                </div>  
-
-
-              </div>
+              <LogBook />
             </div>
           </div>
         <Nav />
