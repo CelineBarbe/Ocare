0bf2c9474e25102da5af2a5a93fff1d68f1c6f12
Zaphaan <jerome.przybylski@gmail.com>
2021-01-29 10:49:11 +0100

    Front : activation of the search bar in the dashboard

--- Front/src/components/Searchbar/index.js
+++ Front/src/components/Searchbar/index.js
@@ -1,20 +1,56 @@
 // == Import npm
-import React from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
+
+import { Link } from 'react-router-dom';
+
+import {returnArrayHandleChange} from 'src/utils/searchAndReturn';
 
 // == Import
 import './searchbar.scss';
 
 
 // == Composant
-const Searchbar = ({changeField, handleSubmit, inputSearchDashboard}) => {
+const Searchbar = ({
+  changeField, 
+  onSubmit, 
+  inputSearchDashboard,
+  list,
+  getPatients,
+}) => {
+
+  useEffect(() => {
+    getPatients();
+  }, [])
+
+const [result,setResult] = useState([]);
+
+  //events
   const handleChange = (evt) => {
     changeField(evt.target.value, evt.target.name);
+    setResult(returnArrayHandleChange(list, evt.target.value));
+    //TODO return patients letter
   };
-  const onSubmitForm = (evt) => {
-    evt.preventDefault();
-    handleSubmit();
-  };
-  return <form className="searchbar" onSubmit={onSubmitForm}>
+
+  const SearchResult = () => {
+    console.log("Resulat dans SearchResult", result);
+    return (
+      <div className="searchResult">
+      {
+        result.length >=1 
+        ? result.map(patient => (
+          <p className="searchResult-name" key={patient.lastname}>
+          <Link to={`/patient/${patient.id}`}>{patient.lastname} {patient.firstname}</Link>
+          </p>
+        ))
+       : null  
+      }
+      </div>
+    )
+  }
+
+  return (
+    <Fragment>
+    <form className="searchbar">
           <input type="text" 
             className="searchbar-input"
             placeholder="Recherche..."
@@ -22,7 +58,10 @@ const Searchbar = ({changeField, handleSubmit, inputSearchDashboard}) => {
             value={inputSearchDashboard}
             onChange={handleChange}
           />
-       </form>
+    </form> 
+    <SearchResult />
+    </Fragment>
+  )
 };
 
 // == Export
