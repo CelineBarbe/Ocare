e7cf752664fd22f5309bfa461880dbae9715d157
LindsayMourinet <lindsay.mourinet@icloud.com>
2021-01-29 15:38:49 +0100

    Add view 081 - update view logbook-with-nurse to add medical_act_name

--- API/app/test.sql
+++ API/app/test.sql
@@ -374,3 +374,17 @@ SELECT l.*,
         JOIN medical_act m
             ON m.id = lhma.medical_act_id
     WHERE l.id = 268;
+
+
+-- Add View logbook with nurse details + medical_act
+SELECT l.*,
+    n.firstname as nurse_firstname,
+    n.lastname as nurse_lastname,
+    m.name as medical_act_name
+    FROM logbook l
+        JOIN nurse n
+            ON n.id = l.nurse_id
+        LEFT OUTER JOIN logbook_has_medical_act lhma
+            ON l.id = lhma.logbook_id
+        LEFT OUTER JOIN medical_act m
+            ON m.id = lhma.medical_act_id;
