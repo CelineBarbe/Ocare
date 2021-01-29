22377761b8dd5e7b2c32adae2d3535f6268df36d
Zaphaan <jerome.przybylski@gmail.com>
2021-01-29 09:53:04 +0100

    Front: create modal for createPage action into patientsPage

--- Front/src/components/App/index.js
+++ Front/src/components/App/index.js
@@ -17,7 +17,6 @@ import CabinetsPage from 'src/containers/CabinetsPage';
 import PatientsPage from 'src/containers/PatientsPage';
 import PatientPage from 'src/containers/PatientPage';
 import CreateCabinet from 'src/containers/CreateCabinet';
-import CreatePatient from 'src/containers/CreatePatient';
 import TourPage from 'src/containers/TourPage';
 import TransmissionPage from 'src/containers/TransmissionPage';
 
@@ -34,7 +33,6 @@ const App = () => (
       <Route exact path="/addcabinet"><CreateCabinet /></Route>
       <Route exact path="/cabinets"><CabinetsPage /></Route>
       <Route exact path="/patients"><PatientsPage /></Route>
-      <Route exact path="/createpatient"><CreatePatient /></Route>
       <Route exact path="/profil"><ProfilPage /></Route>
       <Route path="/patient/:id"><PatientPage /></Route>
       <Route exact path="/tour"><TourPage /></Route>
