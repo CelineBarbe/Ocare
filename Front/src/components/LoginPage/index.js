39c71a226913626369d61327ff50b6ce2931dac3
Zaphaan <jerome.przybylski@gmail.com>
2021-02-04 13:16:12 +0100

    Front : new design

--- Front/src/components/LoginPage/index.js
+++ Front/src/components/LoginPage/index.js
@@ -5,6 +5,7 @@ import {useHistory} from 'react-router-dom';
 import './loginPage.scss';
 
 import Notification from 'src/containers/Notification';
+import logo from 'src/assets/images/logo.svg';
 
 // == Composant
 const LoginPage = ({
@@ -28,28 +29,39 @@ const LoginPage = ({
   };
  
   return (
-    <form className="form" onSubmit={handleSubmit}>
-    <Notification />
-      <input
-        className="form-input"
-        type="email"
-        name="email"
-        placeholder="Email"
-        value={email}
-        onChange={handleChange}
-      />
-      <input
-        className="form-input"
-        type="password"
-        name="password"
-        placeholder="Password"
-        value={password}
-        onChange={handleChange}
-      />
-      <button type="submit" className="form-button" onClick={handleLogin}>
-        Se connecter
-      </button>
-    </form>
+    <div className="login-container">
+      <Notification />
+    <div className="login-container-top">
+      <h1 className="login-container-title">O'CARE</h1>
+      <img src={logo} className="login-container-logo" alt="logo"/>
+    </div>
+    <div className="login-container-bot">
+    <form className="login-container-form" onSubmit={handleSubmit}>
+        <input
+          className="login-container-form-input"
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={email}
+          onChange={handleChange}
+        />
+        <input
+          className="login-container-form-input"
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={password}
+          onChange={handleChange}
+        />
+        <button type="submit" className="login-container-form-button" onClick={handleLogin}>
+          Se connecter
+        </button>
+      </form>
+    </div>
+    
+    
+      
+    </div>
   );
 };
 
