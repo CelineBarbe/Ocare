05a7d2bd748a56448a85657da9fe1d54f343d64f
Zaphaan <jerome.przybylski@gmail.com>
2021-02-05 19:42:44 +0100

    Front: font title dans subtitle

--- Front/src/components/Header/index.js
+++ Front/src/components/Header/index.js
@@ -3,9 +3,6 @@ import React, {useEffect} from 'react';
 import { Link, useHistory } from 'react-router-dom';
 // == Import
 import './header.scss';
-import logo from 'src/assets/images/logo_fin_nude.svg';
-import logo2 from 'src/assets/images/green_logo.svg';
-import logo3 from 'src/assets/images/logo_truecolor_shadowwhite.svg';
 
 import Notification from 'src/containers/Notification';
 
@@ -28,31 +25,18 @@ const Header = ({avatar, listCabinets, default_cabinet, name, changeCabinet, isL
   
   return(
     <header className="header">
-    
-    <ul className="header-ul">
-
-      <li className="header-home">
-        <Link to="/dashboard">
-          <img src={logo2} alt="logo" className="header-img-logo" />
-        </Link>
-      </li>
-    
-      <li className="header-title">
-        <select className="header-title-cabinet" onChange={handleChange}>
+      <Link to="/profil">
+        <img src={avatar} alt="avatar" className="header-img-avatar header-profil" />
+      </Link>
+      <div className="header-title">
+        <select className="header-title-cabinet " onChange={handleChange}>
          <option value={default_cabinet}>{name}</option>
           {!isLoading ? listCabinets.filter(cab => cab.id !== ~~default_cabinet).map(cabinet => (
             <option key={cabinet.id} value={cabinet.id}>{cabinet.name}</option>
           )
           ) : 'data loading...'}
         </select>
-      </li>
-
-      <li className="header-profil">
-        <Link to="/profil">
-          <img src={avatar} alt="avatar" className="header-img-avatar" />
-        </Link>
-      </li>
-    </ul>
+      </div>
     <Notification />
   </header>
   )
