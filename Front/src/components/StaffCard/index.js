d71301abc5a3926cd7955e08c9a7818d0e2e3c10
Zaphaan <jerome.przybylski@gmail.com>
2021-02-06 17:27:58 +0100

    Front : Css cabinetPage

--- Front/src/components/StaffCard/index.js
+++ Front/src/components/StaffCard/index.js
@@ -8,9 +8,10 @@ import './staffCard.scss';
 import AddStaffModal from 'src/containers/AddStaffModal';
 
 // == Import images
-import avatar from 'src/assets/icones/avatar.svg'
-import plus from 'src/assets/icones/plus2.svg';
-import moins from 'src/assets/icones/moinsvert.svg';
+import plus from 'src/assets/icones/plus_blanc.svg';
+import trash from 'src/assets/icones/trash.svg';
+import phone from 'src/assets/icones/phone.svg';
+import mail from 'src/assets/icones/mail.svg';
 
 // == Composant
 const StaffCard = ({
@@ -43,42 +44,52 @@ const StaffCard = ({
   } 
 
   return (
+    <>
     <div className="staff-card">
     { 
       addStaff
       ? <AddStaffModal closeModalAddStaff={closeModalAddStaff} />
       : null
     }
-     {owner_id === idUser ? <img src={plus} alt="stylo" className="staff-card-add" onClick={openModalAddStaff} /> : null}
+     
      <p className="staff-card-title">
        Notre équipe
      </p>
 
      <div className="nurse-card-container">
-
      {
       staff.length>1 
       ? staff.map(equipier => (
       <div className="nurse-card" key={equipier.id}>
           <img src={equipier.avatar} alt="nurse" className="nurse-card-img"/>
-          <p className="nurse-card-infos nurse-card-name">
+          <p className="nurse-card-name">
             {equipier.firstname} {equipier.lastname}
           </p>
-          <p className="nurse-card-infos nurse-card-phone">
-            Tél : <a href={`tel:+33${equipier.phone_number}`}>{equipier.phone_number}</a>
-          </p>
-          <p className="nurse-card-infos nurse-card-mail">
-           <a href={`mailto:${equipier.email}`}>Envoyer un mail</a>
-          </p>
-          {owner_id === idUser ? <img src={moins} alt="moins" className="cabinets-card-add" onClick={ event => getEquipierId(equipier.id)} /> : null}
-      </div>
 
+        <div className="nurse-card-contact"> 
+          <a href={`tel:+33${equipier.phone_number}`}>
+            <img src={phone}  alt="phone" className="nurse-card-contact-img"/>
+          </a>
+          <a href={`mailto:${equipier.email}`}>
+            <img src={mail}  alt="mail" className="nurse-card-contact-img"/>
+           </a> 
+        </div>
+        {owner_id === idUser ? <img src={trash} alt="moins" className="nurse-card-remove" onClick={ event => getEquipierId(equipier.id)} /> : null}
+      </div>
      ))
      : <DefaultComponant/>
      }
       
      </div>
     </div>
+    { owner_id === idUser 
+    ? <div className="button-create-cabinet" onClick={openModalAddStaff} >
+        <span className="button-create-cabinet-title">Inviter un infirmier</span>
+        <img src={plus} alt="croix" className="button-create-cabinet-add"/>
+      </div>
+    : null
+    }
+     </>
   )
 };
 
